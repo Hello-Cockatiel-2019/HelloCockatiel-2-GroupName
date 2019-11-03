47e4d91153aca6880ae3c00881058a7e90d4344b
Pongpichet <pongpichet.sk@mail.kmutt.ac.th>
2019-11-03 20:50:06 +0700

    [ADD] add pic in carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -4,24 +4,30 @@ export default class Carousel extends Component {
     render() {
         return (
             <div>
-                <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="/images/carousel1.png" class="d-block w-50" alt="fox1" />
+                <div class="container">
+                    <div clss="row">
+                        <div clsss="col-lg-4">
+
                         </div>
-                        <div class="carousel-item">
-                            <img src="/images/carousel2.png" class="d-block w-50" alt="fox2" />
-                        </div>
-                        <div class="carousel-item">
-                            <img src="/images/carousel3.png" class="d-block w-50" alt="fox3" />
-                        </div>
-                        <div class="carousel-item">
-                            <img src="/images/carousel4.png" class="d-block w-50" alt="fox4" />
-                        </div>
-                        <div class="carousel-item">
-                            <img src="/images/carousel5.png" class="d-block w-50" alt="fox5" />
-                        </div>
-                    </div>
+                        <div clsss="col-lg-4">
+                            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img src="/images/1.png" class="" alt="fox1" />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="/images/2.png" class="" alt="fox2" />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="/images/3.png" class="" alt="fox3" />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="/images/4.png" class="" alt="fox4" />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img src="/images/5.png" class="" alt="fox5" />
+                                    </div>
+                                </div>
                                 <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
                                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                     <span class="sr-only">Previous</span>
@@ -30,8 +36,14 @@ export default class Carousel extends Component {
                                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span class="sr-only">Next</span>
                                 </a>
+                            </div>
+                        </div>
+                        <div clsss="col-lg-4">
+
+                        </div>
+                    </div>
                 </div>
             </div>
-                        )
-                    }
-                }
+        )
+    }
+}
